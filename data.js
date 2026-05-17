a2f0516a21df175d529734b4d27a6dc160764841
mohiuddinramzan2 <mohiuddinramzan2026@gmail.com>
2026-05-17 09:24:09 +0600

    Initialize fabricCamData with fabric types
    Added fabricCamData object with various fabric types and their corresponding feed configurations.

---
+++ data.js
@@ -0,0 +1,65 @@
+const fabricCamData = {
+    "Single Jersey": {
+        type: "single",
+        feeds: [
+            { cylinder: "knit" },
+            { cylinder: "knit" },
+            { cylinder: "knit" },
+            { cylinder: "knit" }
+        ]
+    },
+    "1x1 Rib": {
+        type: "double",
+        feeds: [
+            { cylinder: "knit", dial: "knit" },
+            { cylinder: "knit", dial: "knit" },
+            { cylinder: "knit", dial: "knit" },
+            { cylinder: "knit", dial: "knit" }
+        ]
+    },
+    "2x2 Rib": {
+        type: "double",
+        feeds: [
+            { cylinder: "knit", dial: "knit" },
+            { cylinder: "knit", dial: "knit" },
+            { cylinder: "miss", dial: "miss" },
+            { cylinder: "miss", dial: "miss" }
+        ]
+    },
+    "Interlock": {
+        type: "double",
+        feeds: [
+            { cylinder: "knit", dial: "miss" },
+            { cylinder: "miss", dial: "knit" },
+            { cylinder: "knit", dial: "miss" },
+            { cylinder: "miss", dial: "knit" }
+        ]
+    },
+    "Pique": {
+        type: "single",
+        feeds: [
+            { cylinder: "knit" },
+            { cylinder: "tuck" },
+            { cylinder: "knit" },
+            { cylinder: "tuck" }
+        ]
+    },
+    "Double Pique": {
+        type: "single",
+        feeds: [
+            { cylinder: "knit" },
+            { cylinder: "tuck" },
+            { cylinder: "tuck" },
+            { cylinder: "knit" }
+        ]
+    },
+    "Jacquard (Simple)": {
+        type: "single",
+        feeds: [
+            { cylinder: "knit" },
+            { cylinder: "miss" },
+            { cylinder: "miss" },
+            { cylinder: "knit" }
+        ]
+    }
+};
